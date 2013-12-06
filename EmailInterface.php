a8333015e9fa592d022674f418a3fe11e528dfac
Amy Stephen <AmyStephen@gmail.com>
2013-12-06 00:19:23 -0600

    Simplify

--- EmailInterface.php
+++ EmailInterface.php
@@ -14,37 +14,15 @@ namespace CommonApi\Email;
  * @package    Email
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  * @copyright  2013 Common Api. All rights reserved.
- * @since      1.0
+ * @since      0.1
  */
 interface EmailInterface
 {
     /**
-     * Return parameter value or default
-     *
-     * @param   string      $key
-     * @param   null|string $default
-     *
-     * @return  mixed
-     * @since   1.0
-     */
-    public function get($key, $default = null);
-
-    /**
-     * Set parameter value
-     *
-     * @param   string     $key
-     * @param   null|mixed $value
-     *
-     * @return  mixed
-     * @since   1.0
-     */
-    public function set($key, $value = null);
-
-    /**
      * Send Email
      *
      * @return  mixed
-     * @since   1.0
+     * @since   0.1
      */
     public function send();
 }
